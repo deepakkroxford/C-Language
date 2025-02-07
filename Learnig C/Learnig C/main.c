1eafb983a07efadcb5cca05168ae4522832b50c8
deepakkroxford <93421333+deepakkroxford@users.noreply.github.com>
2025-02-07 18:06:03 +0530

    fist commit

---
+++ Learnig C/Learnig C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Learnig C
+//
+//  Created by deepakkumar singh on 07/02/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    printf("Hello, World!\n");
+    return 0;
+}
