421d9a3cc70c4586906d6f76234726a4c24f3a93
deepakkroxford <93421333+deepakkroxford@users.noreply.github.com>
2025-02-07 19:37:31 +0530

    ad

---
+++ deepak/deepak/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  deepak
+//
+//  Created by deepakkumar singh on 07/02/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
